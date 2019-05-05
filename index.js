eb7e26b026b5f807076eb66f4299bfb7bcb22626
davydka <davydka@gmail.com>
2019-05-05 11:20:24 -0400

    testing out piping to realtime process inside node

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ process.argv.map(item => {
   }
 });
 
-// ffmpeg -r 60 -f image2 -i seq/%04d.png -crf libx264 -crf 25 -pix_fmt yuv420p out/video.mp4
+// ffmpeg -r 60 -y -f image2 -i seq/%04d.png -crf libx264 -crf 25 -pix_fmt yuv420p out/video.mp4
 // node index.js | glslViewer 07shapes05.frag -w 500 -h 500 -l assets/test2.png > /dev/null 2>&1
 
 
@@ -23,10 +23,13 @@ let tween = new TweenMax.to(o, 1.0, {
   repeat: 0,
   yoyo: true,
   onComplete: function(){
+    console.log('************************ DONE!');
     process.exit();
   }
 })
 
+var glslViewer;
+var i = 0; // global scope
 if(!realtime) {
   tween.paused(true);
   stepRender();
@@ -34,24 +37,46 @@ if(!realtime) {
 
 if(realtime) {
   TweenMax.ticker.addEventListener("tick", render);
+  /*
+  tween.paused(true);
+  glslViewer = spawn('glslViewer', [
+    '07shapes05.frag',
+    '-w',
+    '1920',
+    '-h',
+    '1080',
+    'assets/test2.png',
+  ]);
+  setTimeout(() => {
+    tween.paused(false);
+    TweenMax.ticker.addEventListener("tick", render);
+  }, 500);
+  */
 }
 
 function render() {
   process.stdout.write(`u_temp,${Math.trunc(o.value)}.\n`);
+  /*
+  if(glslViewer.stdin) {
+    const u_temp = spawn('echo', [`u_temp,${Math.trunc(o.value)}.`]);
+
+    u_temp.stdout.pipe(glslViewer.stdin);
+  }
+  */
 }
 
-let i = 0;
 function stepRender() {
   const frameName = String(i).padStart(4, '0');
   const u_temp = spawn('echo', [`u_temp,${Math.trunc(o.value)}.`]);
 
-  const glslViewer = spawn('glslViewer', [
+  glslViewer = spawn('glslViewer', [
     '07shapes05.frag',
     '-w',
     '1920',
     '-h',
     '1080',
     '--headless',
+    'assets/test2.png',
     '-s',
     '1',
     '-o',
